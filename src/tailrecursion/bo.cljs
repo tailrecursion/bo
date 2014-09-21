fc4e653d1d9ec48bc6a0d6195da8ed23332b3d0e
Sreeharsha Mudivarti <msreeharsha@gmail.com>
2014-09-21 19:15:22 +0530

    Example Cleanup

--- src/tailrecursion/bo.cljs
+++ src/tailrecursion/bo.cljs
@@ -79,6 +79,7 @@
 (let [test-bo (BO. (atom {}))]
   (add test-bo :foo {:type :object :bar 1 :behaviors [:blah]})
   (add test-bo :baz {:type :object :car 2 :behaviors [:blah]})
-  (add test-bo :blah {:type :behavior :triggers [:baz] :action (fn [this input] (println input))})
-  (raise test-bo :baz 5))
+  (add test-bo :blah {:type :behavior :triggers [:some-thing]
+                      :action (fn [this input] (println input))})
+  (raise test-bo :some-thing 5))
 
